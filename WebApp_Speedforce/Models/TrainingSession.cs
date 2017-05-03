331347b804bc9828c48e1294bb114931b00d93c9
William <wjhg03@gmail.com>
2017-05-02 21:09:23 -0400

    First commit

---
+++ WebApp_Speedforce/Models/TrainingSession.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+
+namespace WebApp_Speedforce.Models
+{
+    public class TrainingSession
+    {
+        public String SessionID { get; set; }
+        public String UserID { get; set; }
+        public Double AverageBPM { get; set; }
+        public Double BurntCalories { get; set; }
+        public String RouteID { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public Double Distance { get; set; }
+        public Double RelativeHumidity { get; set; }
+        public Double Temperature { get; set; }
+        public String TrainingTypeID { get; set; }
+        public String SessionStatusID { get; set; }
+        public String ClimateConditionID { get; set; }
+
+        public TrainingSession() { }
+
+        public async void GetTrainingSession(string id)
+        {
+            HttpClient client = new HttpClient();
+            TrainingSession ts = new TrainingSession();
+            HttpResponseMessage response = await client.GetAsync("http://speedforceservice.azurewebsites.net/api/training/challenge/"+id);
+            if( response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                
+            }
+
+        }
+    }
+}
---
+++ WebApp_Speedforce/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp_Speedforce.Startup))]
+namespace WebApp_Speedforce
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
