4c086ad2276fd7df2fcd436f9fee53cf7cf4114f
Keiko Tamate <Keiko Tamate>
2017-05-03 21:53:34 -0400

    Se agregó un View de Login para Home. Se agregaron métodos a HomeController para Login y Post-Login el cual hace request de Login de usuario al API de Speedforce. Se agregó un View Model para Login.

--- WebApp_Speedforce/Controllers/HomeController.cs
+++ WebApp_Speedforce/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,6 +27,62 @@ namespace WebApp_Speedforce.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Login to your Speedforce account.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> LoginPost(Models.UserLoginViewModel model)
+        {
+            // URL to Speedforce API
+            string url = "http://speedforceservice.azurewebsites.net/api/users/loginA";
+
+            // JSON Response to Display
+            string responseJson = "N/A";
+
+            // JSON Object to build
+            var json = new JObject();
+
+            // Verifying model
+            if (model.Username == null)
+                model.Username = "N/A";
+            if (model.Password == null)
+                model.Password = "N/A";
+            model.Role = "Atleta";
+
+            // Building JSON to send
+            json["Username"] = model.Username;
+            json["Password"] = model.Password;
+            json["Role"] = model.Role;
+
+            // HTTP transaction
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    using (HttpResponseMessage response = await client.PostAsync(url, new StringContent(json.ToString(), Encoding.UTF8, "application/json")))
+                    {
+                        using (HttpContent content = response.Content)
+                        {
+                            responseJson = await content.ReadAsStringAsync();
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                // If transaction fails
+                return Content("Problemas conectando con Speedforce API.");
+            }
+
+            // If transaction is successful
+            return Content(responseJson, "application/json");
         }
     }
 }
---
+++ WebApp_Speedforce/Models/SpeedforceViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp_Speedforce.Models
+{
+    public class UserLoginViewModel
+    {
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Role")]
+        public string Role { get; set; }
+    }
+}
